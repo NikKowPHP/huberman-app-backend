5bc682de251287c6a52c301e0ce44b2e9adcb68a
NikKowPHP <nik.kow@outlook.com>
2025-06-09 11:23:34 +0200

    feat(auth): Complete P2.7 - Define password reset routes

--- app/Modules/Authentication/routes/api.php
+++ app/Modules/Authentication/routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Modules\Authentication\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail']);
+Route::post('/reset-password', [NewPasswordController::class, 'reset']);
